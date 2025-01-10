33c3c40c320103ff09aa8ddc22670f8715bd9e7c
David-Good-Enough <124247495+David-Good-Enough@users.noreply.github.com>
2025-01-10 15:10:28 +0100

    {~} modification sur sort by date pour ne plus dépendre de boolean

--- src/app/SortByDate.ts
+++ src/app/SortByDate.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Product } from './product';
 
-@Pipe({name: 'SortByDate'})
+@Pipe({ name: 'SortByDate' })
 export class SortbydatepipePipe implements PipeTransform {
-  transform(products: Product[], asc: boolean = true): Product[] {
-    return products.slice().sort((a, b) => { 
-      return asc ? a.createdDate.getTime() - b.createdDate.getTime()
-                 : b.createdDate.getTime() - a.createdDate.getTime();
-    });
+  transform(products: Product[], sortOption: string): Product[] {
+    if (sortOption === 'dateAsc') {
+      return products.slice().sort((a, b) => a.createdDate.getTime() - b.createdDate.getTime());
+    } else if (sortOption === 'dateDesc') {
+      return products.slice().sort((a, b) => b.createdDate.getTime() - a.createdDate.getTime());
+    }
+    return products; // Retourne la liste inchangée si aucun tri par date n'est sélectionné
   }
 }
