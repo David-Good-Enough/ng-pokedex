92f73d1b005701ab78f9c5e463ca0b6ab06c00dd
David-Good-Enough <124247495+David-Good-Enough@users.noreply.github.com>
2025-01-27 09:53:32 +0100

    {+} product service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { SortbydatepipePipe } from './SortByDate';
 import { SearchPipe } from './SearchPipe';
 import { SortByNamePipe } from './SortByNamePipe';
 import { FormsModule } from '@angular/forms';
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -15,7 +16,7 @@ import { FormsModule } from '@angular/forms';
     <h2>Favorite products: {{cuntfavorite}}</h2>
     
     <!-- Barre de recherche -->
-    <input type="text" [(ngModel)]="searchText" placeholder="Rechercher un produit" />
+    <input type="text" [(ngModel)]="searchText" placeholder="Rechercher un personnage" />
 
     <!-- Select pour le tri -->
     <label for="sortOrder">Trier par :</label>
@@ -41,24 +42,9 @@ export class AppComponent {
   title = 'ng-pokedex';
   cuntfavorite = 0;
   searchText = '';
-  sortOption = 'nameAsc'; // Valeur par défaut
-  product = [
-    {id: 0, name: 'Harry Potter', isFavorite: false, createdDate: new Date(1980, 6, 31)},
-    {id: 1, name: 'Ronnald Weasley', isFavorite: false, createdDate: new Date(1980, 3, 1)},
-    {id: 2, name: 'Hermione Granger', isFavorite: false, createdDate: new Date(1979, 8, 19)},
-    {id: 3, name: 'Neville Londubas', isFavorite: false, createdDate: new Date(1980, 7, 30)},
-    {id: 4, name: 'Albus Dumbledore', isFavorite: false, createdDate: new Date(1881, 7, 30)},
-    {id: 5, name: 'Severus Snape', isFavorite: false, createdDate: new Date(1960, 1, 9)},
-    {id: 6, name: 'Draco Malfoy', isFavorite: false, createdDate: new Date(1980, 5, 5)},
-    {id: 7, name: 'Luna Lovegood', isFavorite: false, createdDate: new Date(1981, 2, 13)},
-    {id: 8, name: 'Ginny Weasley', isFavorite: false, createdDate: new Date(1981, 7, 11)},
-    {id: 9, name: 'Fred Weasley', isFavorite: false, createdDate: new Date(1978, 3, 1)},
-    {id: 10, name: 'George Weasley', isFavorite: false, createdDate: new Date(1978, 3, 1)},
-    {id: 11, name: 'Minerva McGonagall', isFavorite: false, createdDate: new Date(1935, 9, 4)},
-    {id: 12, name: 'Hagrid', isFavorite: false, createdDate: new Date(1928, 11, 6)},
-    {id: 13, name: 'Sirius Black', isFavorite: false, createdDate: new Date(1960, 10, 11)},
-    {id: 14, name: 'Remus Lupin', isFavorite: false, createdDate: new Date(1960, 2, 10)}
-  ];
+  sortOption = 'nameAsc';
+  productServ = inject(ProductService);  
+  product = this.productServ.getProducts();
 
   addItem(item: number) {
     this.cuntfavorite += item;
